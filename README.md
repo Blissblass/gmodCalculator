08c62a2104d01a0778b0fd81999b904614669106
Blissblass <lolpo12345@outlook.com>
2021-05-04 04:00:39 +0300

    initial commit: keypad interface

---
+++ README.md
@@ -0,0 +1,2 @@
+Final assignment of TOP's fundamentals course!!!
+This is a Garry's Mod themed keypad!!
---
+++ indexJS.js
@@ -0,0 +1,33 @@
+console.log("Testing! If you see this everythings okay!");
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, op, b) {
+    switch(op) {
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "*":
+            return multiply(a,b);
+        case "/":
+            if(b === 0){
+                return "You can't divide by 0, silly!";
+            }
+            return divide(a,b);            
+    }
+}
