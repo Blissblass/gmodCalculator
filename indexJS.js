a24112657e6b4f92e0c0256202dd677694f5f1e7
Blissblass <lolpo12345@outlook.com>
2021-05-04 14:41:13 +0300

    Added ambience

--- indexJS.js
+++ indexJS.js
@@ -1,6 +1,23 @@
 console.log("Testing! If you see this everythings okay!");
 
-let displayOutput = '';
+let ambienceButton = document.querySelector('#amb-btn');
+let ambience = document.querySelector('#amb');
+ambienceButton.addEventListener('click',play);
+
+// Ambience settings
+let temp = 0;
+function play() {
+    if(temp === 0) {
+        temp = 1;
+        ambience.play();
+    }
+    else if(temp === 1) {
+        temp = 0;
+        ambience.pause();
+    }
+}
+
+let display = document.getElementById('display');
 
 function add(a, b) {
     return a + b;
@@ -36,5 +53,11 @@ function operate(a, op, b) {
 
 let digitBtns = document.querySelectorAll('#digit');
 digitBtns.forEach(btn => {
-    btn.addEventListener('click');
+    btn.addEventListener('click',test);
 });
+
+
+function test(e) {
+    display.textContent = e.target.textContent
+}
+
